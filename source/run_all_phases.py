fe20521da1c7df5ce5fb7d8e0f123fde41dab658
Hui Liu <105711092+hliulab@users.noreply.github.com>
2024-10-24 11:55:38 +0800

    delete previous version

--- source/run_all_phases.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-subprocess.run(["python", "HLA_1.py"])
-
-subprocess.run(["python", "TCR_1.py"])
-
-subprocess.run(["python", "HLA_2.py"])
-
-subprocess.run(["python", "TCR_2.py"])
